6bb12e4509441cc96a5ecbb0f9a3658e12f796fb
John LaTona <johnlatona@Johns-MBP.fios-router.home>
2018-03-14 19:55:38 -0400

    rebuild

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === "production") {
 
 //local route handling
 router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/build/"));
+    res.sendFile(path.join(__dirname, "./client/build/"));
 });
 
 
